79e558caa2730e00624184bccc695e0a80d42ded
Tanpero <3185750139@qq.com>
2019-05-30 22:21:21 +0800

    + update some class to define statements

--- compiler/expression.hh
+++ compiler/expression.hh
@@ -5,25 +5,27 @@
 #include <statement.hh>
 #include <memory>
 
-namespace Flaner::Compiler
+namespace Flaner
 {
-	namespace AST
+	namespace Compiler
 	{
-		class Expression : public Statement
+		namespace AST
 		{
-		public:
+			class Expression : public Statement
+			{
+			public:
 
-		};
+			};
 
-		class BinaryExpreesion : public Expression
-		{
-		public:
-			std::unique_ptr<Expression> left;
-			std::unique_ptr<Expression> right;
-		};
-	}
-}
-
+			class BinaryExpreesion : public Expression
+			{
+			public:
+				std::unique_ptr<Expression> left;
+				std::unique_ptr<Expression> right;
+			};
+		}
+	};
+};
 
 
 #endif // !_FLANER_COMPILER_EXPRESSION_HH_
--- compiler/statement.hh
+++ compiler/statement.hh
@@ -2,17 +2,83 @@
 #define  _FLANER_COMPILER_STATEMENT_HH_
 
 #include <global.hh>
+#include <expression.hh>
+#include <memory>
+#include <vector>
+#include <variant>
+#include <tuple>
 
-namespace Flaner::Compiler
+namespace Flaner
 {
-	namespace AST
+	namespace Compiler
 	{
-		class Statement
+		namespace AST
 		{
-		public:
-			
-		};
-	}
-}
+			class Statement
+			{
+			public:
+
+			};
+
+			class StatementSequence
+			{
+				std::vector<std::unique_ptr<Statement>> sequence;
+			public:
+				std::auto_ptr<Statement> get();
+				bool next();
+				void insert(Statement statement);
+			};
+
+			class IfStatement : public Statement
+			{
+			public:
+				std::unique_ptr<Expression> condition;
+				std::unique_ptr<StatementSequence> body;
+			};
+
+			class WhileStatement : public Statement
+			{
+			public:
+				std::unique_ptr<Expression> condition;
+				std::unique_ptr<StatementSequence> body;
+			};
+
+			class DoWhileStatement : public Statement
+			{
+			public:
+				std::unique_ptr<Expression> condition;
+				std::unique_ptr<StatementSequence> body;
+			};
+
+			class ForStatement : public Statement
+			{
+			public:
+				struct CommonHeader
+				{
+					std::unique_ptr<Expression> initialization;
+					std::unique_ptr<Expression> condition;
+					std::unique_ptr<Expression> increment;
+				};
+
+				// 保留 for...in 和 for...of 语句的循环头定义
+				struct InHeader
+				{
+
+				};
+
+				struct OfHeader
+				{
+
+				};
+
+				using Header = std::variant<CommonHeader, InHeader, OfHeader>;
+
+			public:
+				Header header;
+				std::unique_ptr<StatementSequence> body;
+			};
+		}
+	};
+};
 
 #endif // !_FLANER_COMPILER_STATEMENT_HH_
