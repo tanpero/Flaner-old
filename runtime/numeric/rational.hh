64ee558cc518dee0c6d7535cb177b4c7c8e9b381
Tanpero <3185750139@qq.com>
2019-05-31 23:00:02 +0800

    + implement a bigint class for Runtime

--- runtime/numeric/rational.hh
+++ runtime/numeric/rational.hh
@@ -3,46 +3,57 @@
 
 #include <iostream>
 
-class Rational {
-	friend Rational operator+(const Rational& left, const Rational& right);
-	friend Rational operator-(const Rational& left, const Rational& right);
-	friend Rational operator*(const Rational& left, const Rational& right);
-	friend Rational operator/(const Rational& left, const Rational& right);
-	friend bool operator==(const Rational& left, const Rational& right);
-	friend bool operator!=(const Rational& left, const Rational& right);
-	friend bool operator<(const Rational& left, const Rational& right);
-	friend bool operator>(const Rational& left, const Rational& right);
-	friend bool operator<=(const Rational& left, const Rational& right);
-	friend bool operator>=(const Rational& left, const Rational& right);
-	friend std::ostream& operator<<(std::ostream& out, const Rational& obj);
-	friend std::istream& operator>>(std::istream& in, Rational& obj);
+namespace Flaner
+{
+	namespace Runtime
+	{
+		namespace numeric
+		{
 
-public:
-	Rational();
-	Rational(double x);
-	Rational(int numerator_, int denominator_ = 1);
+			class Rational {
+				friend Rational operator+(const Rational& left, const Rational& right);
+				friend Rational operator-(const Rational& left, const Rational& right);
+				friend Rational operator*(const Rational& left, const Rational& right);
+				friend Rational operator/(const Rational& left, const Rational& right);
+				friend bool operator==(const Rational& left, const Rational& right);
+				friend bool operator!=(const Rational& left, const Rational& right);
+				friend bool operator<(const Rational& left, const Rational& right);
+				friend bool operator>(const Rational& left, const Rational& right);
+				friend bool operator<=(const Rational& left, const Rational& right);
+				friend bool operator>=(const Rational& left, const Rational& right);
+				friend std::ostream& operator<<(std::ostream& out, const Rational& obj);
+				friend std::istream& operator>>(std::istream& in, Rational& obj);
 
-	Rational& operator=(const Rational& obj);
-	Rational& operator+=(const Rational& obj);
-	Rational& operator-=(const Rational& obj);
-	Rational& operator*=(const Rational& obj);
-	Rational& operator/=(const Rational& obj);
-	Rational& operator++();
-	Rational operator++(int);
-	Rational& operator--();
-	Rational operator--(int);
-	Rational operator+() const;
-	Rational operator-() const;
+			public:
+				Rational();
+				Rational(double x);
+				Rational(int numerator_, int denominator_ = 1);
 
-	void setNumerator(int numerator_);
-	int getNumerator() const;
-	void setDenominator(int denominator_);
-	int getDenominator() const;
+				Rational& operator=(const Rational& obj);
+				Rational& operator+=(const Rational& obj);
+				Rational& operator-=(const Rational& obj);
+				Rational& operator*=(const Rational& obj);
+				Rational& operator/=(const Rational& obj);
+				Rational& operator++();
+				Rational operator++(int);
+				Rational& operator--();
+				Rational operator--(int);
+				Rational operator+() const;
+				Rational operator-() const;
 
-private:
-	int numerator;
-	int denominator;
-	void simplify();
+				void setNumerator(int numerator_);
+				int getNumerator() const;
+				void setDenominator(int denominator_);
+				int getDenominator() const;
+
+			private:
+				int numerator;
+				int denominator;
+				void simplify();
+			};
+
+		};
+	};
 };
 
 #endif // !_FLANER_RUNTIME_NUMERIC_RATIONAL_HH_
