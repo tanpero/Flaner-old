7117d71cbdb6cc703a63cee7335ad79a68accdc0
Tanpero <3185750139@qq.com>
2019-06-01 06:49:05 +0800

    + add a batch of numerical algorithm is suitable for numeric::Bigint

--- runtime/numeric/bigint.cc
+++ runtime/numeric/bigint.cc
@@ -9,56 +9,124 @@ namespace Flaner
 
 			Bigint::Bigint()
 			{
+				sign = 1;
 			}
 
 
 			Bigint::Bigint(long long v)
 			{
+				*this = v;
 			}
 
 
 			Bigint::Bigint(std::string & s)
 			{
+				read(s);
 			}
 
 
 			void Bigint::operator=(const Bigint &v)
 			{
+				sign = v.sign;
+				a = v.a;
 			}
 
 
 			void Bigint::operator=(long long v)
 			{
+				sign = 1;
+				a.clear();
+				if (v < 0)
+					sign = -1, v = -v;
+				for (; v > 0; v = v / base)
+					a.push_back(v % base);
 			}
 
 
 			Bigint Bigint::operator+(const Bigint &v) const
 			{
+				if (sign == v.sign) {
+					Bigint res = v;
+
+					for (int i = 0, carry = 0; i < (int)std::max(a.size(), v.a.size()) || carry; ++i) {
+						if (i == (int)res.a.size())
+							res.a.push_back(0);
+						res.a[i] += carry + (i < (int)a.size() ? a[i] : 0);
+						carry = res.a[i] >= base;
+						if (carry)
+							res.a[i] -= base;
+					}
+					return res;
+				}
+				return *this - (-v);
 			}
 
 
 			Bigint Bigint::operator-(const Bigint &v) const
 			{
+				if (sign == v.sign) {
+					if (abs() >= v.abs()) {
+						Bigint res = *this;
+						for (int i = 0, carry = 0; i < (int)v.a.size() || carry; ++i) {
+							res.a[i] -= carry + (i < (int)v.a.size() ? v.a[i] : 0);
+							carry = res.a[i] < 0;
+							if (carry)
+								res.a[i] += base;
+						}
+						res.trim();
+						return res;
+					}
+					return -(v - *this);
+				}
+				return *this + (-v);
 			}
 
 
 			void Bigint::operator*=(int v)
 			{
+				if (v < 0)
+					sign = -sign, v = -v;
+				for (int i = 0, carry = 0; i < (int)a.size() || carry; ++i) {
+					if (i == (int)a.size())
+						a.push_back(0);
+					long long cur = a[i] * (long long)v + carry;
+					carry = (int)(cur / base);
+					a[i] = (int)(cur % base);
+
+				}
+				trim();
 			}
 
 
 			Bigint Bigint::operator*(int v) const
 			{
+				Bigint res = *this;
+				res *= v;
+				return res;
 			}
 
 
 			void Bigint::operator*=(long long v)
 			{
+				if (v < 0)
+					sign = -sign, v = -v;
+				for (int i = 0, carry = 0; i < (int)a.size() || carry; ++i) {
+					if (i == (int)a.size())
+						a.push_back(0);
+					long long cur = a[i] * (long long)v + carry;
+					carry = (int)(cur / base);
+					a[i] = (int)(cur % base);
+
+				}
+				trim();
 			}
 
 
 			Bigint Bigint::operator*(long long v) const
 			{
+				Bigint res = *this;
+				res *= v;
+				return res;
 			}
 
 
@@ -104,122 +172,198 @@ namespace Flaner
 			}
 
 
-			std::pair<Bigint, Bigint> Bigint::divmod(const Bigint &a1, const Bigint &b1)
+			std::pair<Bigint, Bigint> divmod(const Bigint & a1, const Bigint & b1)
 			{
-			}
 
+				int norm = base / (b1.a.back() + 1);
+				Bigint a = a1.abs() * norm;
+				Bigint b = b1.abs() * norm;
+				Bigint q, r;
+				q.a.resize(a.a.size());
+
+				for (int i = a.a.size() - 1; i >= 0; i--) {
+					r *= base;
+					r += a.a[i];
+					int s1 = r.a.size() <= b.a.size() ? 0 : r.a[b.a.size()];
+					int s2 = r.a.size() <= b.a.size() - 1 ? 0 : r.a[b.a.size() - 1];
+					int d = ((long long)base * s1 + s2) / b.a.back();
+					r -= b * d;
+					while (r < 0)
+						r += b, --d;
+					q.a[i] = d;
+				}
+
+				q.sign = a1.sign * b1.sign;
+				r.sign = a1.sign;
+				q.trim();
+				r.trim();
+				return std::make_pair(q, r / norm);
+			}
 
 
 			Bigint Bigint::operator/(const Bigint &v) const
 			{
+				return divmod(*this, v).first;
 			}
 
 
 			Bigint Bigint::operator%(const Bigint &v) const
 			{
+				return divmod(*this, v).second;
 			}
 
 
 			void Bigint::operator/=(int v)
 			{
+				if (v < 0)
+					sign = -sign, v = -v;
+				for (int i = (int)a.size() - 1, rem = 0; i >= 0; --i) {
+					long long cur = a[i] + rem * (long long)base;
+					a[i] = (int)(cur / v);
+					rem = (int)(cur % v);
+				}
+				trim();
 			}
 
 
 			Bigint Bigint::operator/(int v) const
 			{
+				Bigint res = *this;
+				res /= v;
+				return res;
 			}
 
 
 			int Bigint::operator%(int v) const
 			{
+				if (v < 0)
+					v = -v;
+				int m = 0;
+				for (int i = a.size() - 1; i >= 0; --i)
+					m = (a[i] + m * (long long)base) % v;
+				return m * sign;
 			}
 
 
 
 			void Bigint::operator+=(const Bigint &v)
 			{
+				*this = *this + v;
 			}
 
 
 			void Bigint::operator-=(const Bigint &v)
 			{
+				*this = *this - v;
 			}
 
 
 			void Bigint::operator*=(const Bigint &v)
 			{
+				*this = *this * v;
 			}
 
 
 			void Bigint::operator/=(const Bigint &v)
 			{
+				*this = *this / v;
 			}
 
 
 
 			bool Bigint::operator<(const Bigint &v) const
 			{
+				if (sign != v.sign)
+					return sign < v.sign;
+				if (a.size() != v.a.size())
+					return a.size() * sign < v.a.size() * v.sign;
+				for (int i = a.size() - 1; i >= 0; i--)
+					if (a[i] != v.a[i])
+						return a[i] * sign < v.a[i] * sign;
+				return false;
 			}
 
 
 			bool Bigint::operator>(const Bigint &v) const
 			{
+				return v < *this;
 			}
 
 
 			bool Bigint::operator<=(const Bigint &v) const
 			{
+				return !(v < *this);
 			}
 
 
 			bool Bigint::operator>=(const Bigint &v) const
 			{
+				return !(*this < v);
 			}
 
 
 			bool Bigint::operator==(const Bigint &v) const
 			{
+				return !(*this < v) && !(v < *this);
 			}
 
 
 			bool Bigint::operator!=(const Bigint &v) const
 			{
+				return *this < v || v < *this;
 			}
 
 
 			void Bigint::trim()
 			{
+				while (!a.empty() && !a.back())
+					a.pop_back();
+				if (a.empty())
+					sign = 1;
 			}
 
 
 			bool Bigint::isZero() const
 			{
+				return a.empty() || (a.size() == 1 && !a[0]);
 			}
 
 
 			Bigint Bigint::operator-() const
 			{
+				Bigint res = *this;
+				res.sign = -sign;
+				return res;
 			}
 
 
 			Bigint Bigint::abs() const
 			{
+				Bigint res = *this;
+				res.sign *= res.sign;
+				return res;
 			}
 
 
 
 			long long Bigint::longValue() const
 			{
+				long long res = 0;
+				for (int i = a.size() - 1; i >= 0; i--)
+					res = res * base + a[i];
+				return res * sign;
 			}
 
 
-			Bigint Bigint::gcd(const Bigint &a, const Bigint &b)
+			Bigint gcd(const Bigint &a, const Bigint &b)
 			{
+				return b.isZero() ? a : gcd(b, a % b);
 			}
 
 
-			Bigint Bigint::lcm(const Bigint &a, const Bigint &b)
+			Bigint lcm(const Bigint & a, const Bigint & b)
 			{
+				return a / gcd(a, b) * b;
 			}
 
 
@@ -227,7 +371,6 @@ namespace Flaner
 			void Bigint::read(const std::string &s)
 			{
 			}
-
 
 
 			std::istream& operator>>(std::istream &stream, Bigint &v)
