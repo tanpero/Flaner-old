e7ff742005765a9e0efb586c0f1a825d95869672
tanpero <3185750139@qq.com>
2019-06-23 09:47:25 +0800

    + add class Runtime::Concurrency::FunctionWrapper

--- runtime/concurrency/detail/functionWrapper.hh
+++ runtime/concurrency/detail/functionWrapper.hh
@@ -2,6 +2,7 @@
 #define _FLANER_RUNTIME_CONCURRENT_DETAIL_FUNCTIONWRAPPER_HH_
 
 #include <global.hh>
+#include <memory>
 
 namespace Flaner
 {
@@ -15,8 +16,44 @@ namespace Flaner
 				{
 					struct ImplBase
 					{
-
+						virtual void call() = 0;
+						virtual ~ImplBase() {}
 					};
+
+					std::shared_ptr<ImplBase> implBase;
+
+					template <typename F>
+					struct ImplType : ImplBase
+					{
+						F f;
+						ImplType(F&& _f) : f(std::move(_f)) {}
+						void call()
+						{
+							f();
+						}
+					};
+
+				public:
+					template <typename F>
+					FunctionWrapper(F&& f) :
+						implBase(new ImplType<F>(std::move(f)))
+					{}
+					void operator()()
+					{
+						implBase->call();
+					}
+					FunctionWrapper() = default;
+					FunctionWrapper(FunctionWrapper&& other) :
+						implBase(std::move(other.implBase))
+					{}
+					FunctionWrapper& operator=(FunctionWrapper&& other)
+					{
+						implBase = std::move(other.implBase);
+						return *this;
+					}
+					FunctionWrapper(const FunctionWrapper&) = delete;
+					FunctionWrapper(FunctionWrapper&) = delete;
+					FunctionWrapper& operator=(const FunctionWrapper&) = delete;
 				};
 			}
 		}
