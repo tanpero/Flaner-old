ca29fb454182edf88e0850f88765e53dc11dff8b
tanpero <3185750139@qq.com>
2019-06-23 10:06:04 +0800

    + add class Runtime::Concurrency::WorkStealingQueue

---
+++ runtime/concurrency/detail/workStealingQueue.hh
@@ -0,0 +1,73 @@
+#ifndef _FLANER_RUNTIME_CONCURRENCY_DETAIL_WORJKSTEALINGQUEUE_HH_
+#define _FLANER_RUNTIME_CONCURRENCY_DETAIL_WORJKSTEALINGQUEUE_HH_
+
+#include <global.hh>
+#include <functionWrapper.hh>
+#include <deque>
+#include <mutex>
+
+namespace Flaner
+{
+	namespace Runtime
+	{
+		namespace Concurrency
+		{
+			namespace detail
+			{
+				class WorkStealingQueue
+				{
+					using DataType = FunctionWrapper;
+
+					std::deque<DataType> theQueue;
+					mutable std::mutex theMutex;
+
+				public:
+					WorkStealingQueue()
+					{}
+
+					WorkStealingQueue(const WorkStealingQueue& other) = delete;
+					WorkStealingQueue& operator=
+						(const WorkStealingQueue& other) = delete;
+
+					void push(DataType data)
+					{
+						std::lock_guard<std::mutex> lock(theMutex);
+						theQueue.push_front(std::move(data));
+					}
+
+					bool isEmpty() const
+					{
+						std::lock_guard<std::mutex> lock(theMutex);
+						return theQueue.empty();
+					}
+
+					bool tryPop(DataType& res)
+					{
+						std::lock_guard<std::mutex> lock(theMutex);
+						if (theQueue.empty())
+						{
+							return false;
+						}
+						res = std::move(theQueue.front());
+						theQueue.pop_front();
+						return true;
+					}
+
+					bool trySteal(DataType& res)
+					{
+						std::lock_guard<std::mutex> lock(theMutex);
+						if (theQueue.empty())
+						{
+							return false;
+						}
+						res = std::move(theQueue.back());
+						theQueue.pop_back();
+						return true;
+					}
+				};
+			}
+		}
+	}
+}
+
+#endif // !_FLANER_RUNTIME_CONCURRENCY_DETAIL_WORJKSTEALINGQUEUE_HH_
